5779c1f268bee5f7e7bb041bc3d3dc6b266173b7
Matei <grama.matei.george@gmail.com>
2019-05-22 16:07:21 +0100

    Request and response test testing.

--- application.py
+++ application.py
@@ -1,6 +1,30 @@
+import json
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Buenos dias!"
+
+
+@app.route("/login")
+def login():
+    username = request.args.get('username')
+    password = request.args.get('password')
+
+    result= {"text": "test request & response for" + username}
+
+    return json.dumps(result)
+
+
+@app.route("/register")
+def register():
+    username = request.args.get('username')
+    firstname = request.args.get('firstname')
+    lastname = request.args.get('lastname')
+
+
+    return json.dumps(result)
+
