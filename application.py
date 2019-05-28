f42469d0171ffef81e210d22394c3966a11cc8c2
Matei Grama <grama.matei.george@gmail.com>
2019-05-28 12:14:18 +0100

    Update application.py

--- application.py
+++ application.py
@@ -5,10 +5,18 @@ from flask import Flask
 from flask import request
 
 app = Flask(__name__)
-
+"""
 #connectionString = "Driver={ODBC Driver 13 for SQL Server};Server=tcp:webapp-db-sv.database.windows.net,1433;Database=WebAppDb;Uid=BoneyHadger@webapp-db-sv;Pwd=HoneyBadger123$;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
 #cnxn = pyodbc.connect(connectionString)
 #cursor = cnxn.cursor()
+"""
+drivers = [item for item in pyodbc.drivers()]
+driver = drivers[-1]
+print("driver:{}".format(driver))
+con_string = f'DRIVER={driver};Server=tcp:webapp-db-sv.database.windows.net,1433;Database=WebAppDb;Uid=BoneyHadger@webapp-db-sv;Pwd=HoneyBadger123$;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+print(con_string)
+cnxn = pyodbc.connect(con_string)
+
 
 @app.route("/")
 def hello():
