d46de4181817c1dd7a8d4258c081a40a5cf4a53c
Matei Grama <grama.matei.george@gmail.com>
2019-05-29 18:28:56 +0100

    Added upload apk API.

--- application.py
+++ application.py
@@ -1,10 +1,19 @@
 import json
 import pyodbc
+import os
 
-from flask import Flask
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 from flask import request
 
+UPLOAD_FOLDER = '/home/site/wwwroot/uploads'
+
 app = Flask(__name__)
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['.apk'])
 
 drivers = [item for item in pyodbc.drivers()]
 driver = drivers[-1]
@@ -12,7 +21,11 @@ con_string = "DRIVER={};Server=tcp:webapp-db-sv.database.windows.net,1433;Databa
 print(con_string)
 cnxn = pyodbc.connect(con_string)
 cursor = cnxn.cursor()
-    
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+	
+
 @app.route("/")
 def hello():
     try:
@@ -38,3 +51,24 @@ def register():
     lastname = request.args.get('lastname')
     return "register"
 
+
+@app.route('/uploadAPK', methods=['POST'])
+def upload_file():
+
+    if(request.args.get('key')!= 'muieDragnea'):
+        return "n-ai parola, nu pui sus."
+
+	if request.method == 'POST':
+        # check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			return redirect(request.url)
+		file = request.files['file']
+		if file.filename == '':
+			flash('No file selected for uploading')
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			flash('File(s) successfully uploaded')
+			return redirect('/')
