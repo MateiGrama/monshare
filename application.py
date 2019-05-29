517e985a8efd4eed6c5891de47e2ecace93bb26b
Matei Grama <grama.matei.george@gmail.com>
2019-05-29 18:38:46 +0100

    Using spaces instead of tabs.

--- application.py
+++ application.py
@@ -23,8 +23,8 @@ cnxn = pyodbc.connect(con_string)
 cursor = cnxn.cursor()
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    
 
 @app.route("/")
 def hello():
@@ -58,17 +58,17 @@ def upload_file():
     if(request.args.get('key')!= 'muieDragnea'):
         return "n-ai parola, nu pui sus."
 
-	if request.method == 'POST':
+    if request.method == 'POST':
         # check if the post request has the file part
-		if 'file' not in request.files:
-			flash('No file part')
-			return redirect(request.url)
-		file = request.files['file']
-		if file.filename == '':
-			flash('No file selected for uploading')
-			return redirect(request.url)
-		if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-			flash('File(s) successfully uploaded')
-			return redirect('/')
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File(s) successfully uploaded')
+            return redirect('/')
