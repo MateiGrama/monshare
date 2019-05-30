431dc477604eef6ceeb926e5e45589f1f04a7f12
Matei Grama <grama.matei.george@gmail.com>
2019-05-30 19:26:12 +0100

    Implemented getGroupsAround API.

--- application.py
+++ application.py
@@ -5,6 +5,7 @@ import os
 from flask import Flask, flash, request, redirect, render_template
 from werkzeug.utils import secure_filename
 from flask import request
+import datetime
 
 UPLOAD_FOLDER = '/home/site/wwwroot/uploads'
 FAIL_STATUS = "fail"
@@ -61,25 +62,40 @@ def createGroup():
 
     if not user_id or not session_id:
         return error_status_response("invalid id or sessionid")
-
     
-    cursor.execute("select * from Users where userid={} and sessionid={}".format(user_id, session_id))
-    
-    if not cursor.fetchall():
-        return error_status_response("user has not got a valid session id")
+    line = 0
+    try:
+        if check_login(user_id, session_id):
+            return authentification_failed()
+        
+        line += 1
+        result = cursor.execute("insert into groups (title, description,creationdatetime, ownerId) values ('{}','{}',GETDATE(), {})".format(group_name, group_description ,user_id))
+        cnxn.commit()
+        if result:
+            return success_status()
 
-    result = cursor.execute("insert into groups (title, description,creationdatetime, ownerId) values ({},{},GETDATE(), {})".format(group_name, group_description ,user_id))
+    except:
+        return error_status_response("error while inserting group in db; line: " + str(line))
 
-    if result:
-        return success_status()
-    
-    return error_status_response("error while inserting group in db")
-
-
-    
+   
 @app.route("/getGroupsAround")
 def getGroupsAround():
-    pass
+    user_id = request.args.get('user_id')
+    session_id = request.args.get('session_id')
+
+    if not user_id or not session_id:
+        return error_status_response("invalid id or sessionid")
+    try:
+       if check_login(user_id, session_id):
+           return authentification_failed()
+       cursor.execute("select * from groups")
+       columns = [column_description[0] for column_description in cursor.description]
+       rows = cursor.fetchall()
+    except:
+        return error_status_response("error while getting all groups; line:" + str(line))
+    
+    return group_list_to_json(rows, columns)
+
 
 @app.route("/joinGroup")
 def joinGroup():
@@ -88,6 +104,26 @@ def joinGroup():
 def leaveGroup():
     pass
 
+def group_list_to_json(rows, columns):
+    try:
+        groups = []
+        for row in rows:
+            if (not row[3] is None):
+                row[3] = row[3].strftime('%Y-%m-%dT%H:%M:%S.%f')
+            if (not row[4] is None):
+                row[4] = row[4].strftime('%Y-%m-%dT%H:%M:%S.%f')
+            groups.append(dict(zip(columns, row)))
+        result = {'status':'success' , 'groups' : json.dumps(groups)}
+        return json.dumps(result)
+    except:
+        return error_status_response("error while generating json for rows; line:" + str(line))
+
+def check_login(user_id, session_id):
+    cursor.execute("select * from Users where userid={} and sessionid={}".format(user_id, session_id))
+    return not cursor.fetchall()
+
+def authentification_failed():
+    return error_status_response("user has not got a valid session id")
 
 def success_status():
    result = {"status" : SUCCESS_STATUS}
