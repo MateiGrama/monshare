943d83336e90da67fe615875d179445279cfa5c4
Matei Grama <grama.matei.george@gmail.com>
2019-05-31 17:34:17 +0100

    Register fronted form implemneted.

---
+++ monshare/monshare/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Text;
+using monshare.Utils;
+
+namespace monshare.Models
+{
+    class User : NullObject<User>
+    {
+        public string  firstName{ get; internal set; }
+        public string  lastName{ get; internal set; }
+        public string  userId{ get; internal set; }
+        public string  email{ get; internal set; }
+    }
+}
