a13073f1c841d9323cb3c3678829a4d55ad61243
Victor-Gabriel Apostol <vg.apostol@yahoo.com>
2019-06-01 20:58:35 +0100

    Cleaned up code in application.py

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import json
-from random import randint
 
 import pyodbc
 from flask import Flask
@@ -8,12 +7,11 @@ from flask import request
 from utils import keys, db_functionalities
 from utils.db_functionalities import is_user_member_of_group, group_has_one_member, delete_group, is_group_owner, \
     pass_ownership, remove_user_from_group
-from utils.utils import get_fields
+from utils.utils import get_fields, error_status_response, SUCCESS_STATUS, logged_in, unauthorized_user, success_status, \
+    get_random_SSID, group_list_to_json
 
 DEBUG = False
 UPLOAD_FOLDER = '/home/site/wwwroot/uploads'
-FAIL_STATUS = "fail"
-SUCCESS_STATUS = "success"
 
 app = Flask(__name__)
 
@@ -31,17 +29,48 @@ def welcome_page():
     return "Welcome to MonShare"
 
 
+@app.route("/register")
+def register():
+    email, first_name, last_name, password_hash = get_fields('email', 'first_name', 'last_name', 'password_hash')
+
+    if not email or not password_hash or not first_name or not last_name:
+        return error_status_response("Provided input is not valid.")
+
+    # Check that user is in database
+    try:
+        cursor.execute("SELECT * FROM users WHERE email = '{}';".format(email))
+        if len(cursor.fetchall()) > 0:
+            return error_status_response("Email already in use.")
+
+        cursor.execute("""INSERT INTO users (firstname, lastname, passwordhash, sessionId, email) 
+                          values ('{}','{}','{}','{}','{}')
+                       """.format(first_name, last_name, password_hash, get_random_SSID(), email))
+        connection.commit()
+
+        # Return success result
+        cursor.execute("SELECT * FROM users WHERE email = '{}';".format(email))
+        user_details = cursor.fetchone()
+
+        result = {"status": SUCCESS_STATUS, "user": {"user_id": user_details.UserId,
+                                                     "session_id": user_details.SessionId,
+                                                     "first_name": user_details.FirstName,
+                                                     "last_name": user_details.LastName}}
+
+    except:
+        return error_status_response("Error while processing register request.")
+    return json.dumps(result)
+
+
 @app.route("/login")
 def login():
-    email = request.args.get('email')
-    password_hash = request.args.get('password_hash')
+    email, password_hash = get_fields('email', 'password_hash')
 
-    if not email or not password_hash:
+    if not (email and password_hash):
         return error_status_response("No email or password provided.")
 
     # Check that user is in database
     try:
-        cursor.execute("SELECT SessionId, PasswordHash FROM users WHERE email = '{}';".format(email));
+        cursor.execute("SELECT SessionId, PasswordHash FROM users WHERE email = '{}';".format(email))
         user_details = cursor.fetchone()
 
         if not user_details:
@@ -49,8 +78,9 @@ def login():
         if not user_details.PasswordHash == password_hash:
             return error_status_response("Wrong password provided.")
 
-        cursor.execute(
-            "UPDATE users SET sessionId = {} WHERE email = '{}'".format(int(user_details.SessionId) + 1, email))
+        cursor.execute("""UPDATE users SET sessionId = {} 
+                          WHERE email = '{}'
+                       """.format(int(user_details.SessionId) + 1, email))
         connection.commit()
 
         # Return success result
@@ -63,16 +93,15 @@ def login():
                                                      "last_name": user_details.LastName}}
 
     except:
-        return error_status_response("error while processing login request")
+        return error_status_response("Error while processing login request.")
     return json.dumps(result)
 
 
 @app.route("/logout")
 def logout():
-    user_id = request.args.get('user_id')
-    session_id = request.args.get('session_id')
+    user_id, session_id = get_fields('user_id', 'session_id')
 
-    if not user_id or not session_id:
+    if not (user_id and session_id):
         return error_status_response("No user_id or session_id provided.")
 
     # Check that the connection is valid
@@ -80,56 +109,16 @@ def logout():
         if not logged_in(user_id, session_id):
             return unauthorized_user()
 
-        cursor.execute("UPDATE users SET sessionId = {} WHERE userId = {}".format(getRandomSSID(), user_id))
+        cursor.execute("UPDATE users SET sessionId = {} WHERE userId = {}".format(get_random_SSID(), user_id))
         connection.commit()
 
     except:
-        return error_status_response("error while processing login request")
+        return error_status_response("Error while processing logout request.")
     return success_status()
 
 
-@app.route("/register")
-def register():
-    email = request.args.get('email')
-    first_name = request.args.get('first_name')
-    last_name  = request.args.get('last_name')
-    password_hash  = request.args.get('password_hash')
-
-    if not email or not password_hash or not first_name or not last_name:
-        return error_status_response("Provided input is not valid.")
-    line = 0
-    # Check that user is in database
-    try:
-        line += 1
-        cursor.execute("SELECT * FROM users WHERE email = '{}';".format(email));
-        if len(cursor.fetchall()) > 0:
-            return error_status_response("Email already in use.")
-        line += 1
-
-        cursor.execute("INSERT INTO users (firstname, lastname, passwordhash, sessionId, email) values ('{}','{}','{}','{}','{}')".format(first_name, last_name, password_hash, getRandomSSID(), email))
-        connection.commit()
-        line += 1
-
-        # Return success result
-        cursor.execute("SELECT * FROM users WHERE email = '{}';".format(email))
-        line += 1
-        user_details = cursor.fetchone()
-        line += 1
-
-        result = {"status": SUCCESS_STATUS, "user": {"user_id": user_details.UserId,
-                                                     "session_id": user_details.SessionId,
-                                                     "first_name": user_details.FirstName,
-                                                     "last_name": user_details.LastName}}
-        line += 1
-
-
-    except:
-        return error_status_response("error while processing register request, line:" + str(line))
-    return json.dumps(result)
-
-
 @app.route("/createGroup")
-def createGroup():
+def create_group():
     user_id = request.args.get('user_id')
     session_id = request.args.get('session_id')
     group_name = request.args.get('group_name')
@@ -142,9 +131,9 @@ def createGroup():
         if logged_in(user_id, session_id):
             return unauthorized_user()
 
-        result = cursor.execute(
-            """insert into groups (title, description, creationdatetime, ownerId)
-               values ('{}','{}',GETDATE(), {})""".format(group_name, group_description, user_id))
+        result = cursor.execute("""insert into groups (title, description, creationdatetime, ownerId)
+                                   values ('{}', '{}', GETDATE(), {})
+                                """.format(group_name, group_description, user_id))
         connection.commit()
 
         if result:
@@ -215,40 +204,3 @@ def leave_group(*args):
 @app.route("/deleteAccount")
 def delete_account():
     pass
-
-
-def group_list_to_json(rows, columns):
-    try:
-        groups = []
-        for row in rows:
-            if not row[3] is None:
-                row[3] = row[3].strftime('%Y-%m-%dT%H:%M:%S.%f')
-            if not row[4] is None:
-                row[4] = row[4].strftime('%Y-%m-%dT%H:%M:%S.%f')
-            groups.append(dict(zip(columns, row)))
-        result = {'status': 'success', 'groups': groups}
-        return json.dumps(result)
-    except:
-        return error_status_response("error while generating json for rows")
-
-
-def logged_in(user_id, session_id):
-    cursor.execute("select * from Users where userid={} and sessionid={}".format(user_id, session_id))
-    return cursor.fetchall()
-
-
-def unauthorized_user():
-    return error_status_response("user has not got a valid session id")
-
-
-def success_status(msg):
-    result = {"message": msg, "status": SUCCESS_STATUS}
-    return json.dumps(result)
-
-
-def error_status_response(msg):
-    result = {"description": msg, "status": FAIL_STATUS}
-    return json.dumps(result)
-
-def getRandomSSID():
-    return randint(1,100000000);
