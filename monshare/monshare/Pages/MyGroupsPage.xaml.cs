8706a83adf629ca328d52ffdb588175c8f495ccb
Victor-Gabriel Apostol <vg.apostol@yahoo.com>
2019-06-04 00:12:21 +0100

    Let user create a new group new My Groups button clicked and the user is not part of any groups.

--- monshare/monshare/Pages/MyGroupsPage.xaml.cs
+++ monshare/monshare/Pages/MyGroupsPage.xaml.cs
@@ -14,8 +14,8 @@ namespace monshare.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MyGroupsPage : ContentPage
     {
-
         private List<Group> Groups;
+
         public MyGroupsPage()
         {
             InitializeComponent();
@@ -29,12 +29,37 @@ namespace monshare.Pages
         private async void RefreshMyGroupsAsync()
         {
             Groups = await ServerCommunication.GetMyGroupsAsync();
-            GroupsListView.ItemsSource = Groups;
+            if (Groups.Count > 0)
+            {
+                GroupsListView.ItemsSource = Groups;
+            }
+            else
+            {
+                Button button = new Button
+                {
+                    Text = "Create a new group"
+                };
+                button.Clicked += (s, e) => CreateNewGroupButtonPressed(s, e);
+                //TODO: Fix this
+                groupsStackLayout.Children.Add(new Label()
+                {
+                    Text = "You don't have any grups yet! Create a new group by clicking on the button below.",
+                    HorizontalOptions = LayoutOptions.CenterAndExpand,
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                    Opacity = 0.75,
+                    FontSize = 16
+                });
+            }
         }
 
         private async void GroupTapped(object sender, SelectedItemChangedEventArgs e)
         {
             await Navigation.PushAsync(new GroupDescriptionPage(Groups[e.SelectedItemIndex]));
+        }
+
+        private async void CreateNewGroupButtonPressed(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreateGroupPage());
         }
     }
 }
