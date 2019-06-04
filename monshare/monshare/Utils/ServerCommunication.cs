568d26df31861d1522f3f53004b36000485029ec
Victor-Gabriel Apostol <vg.apostol@yahoo.com>
2019-06-04 02:23:04 +0100

    Moved function in Utils module.

--- monshare/monshare/Utils/ServerCommunication.cs
+++ monshare/monshare/Utils/ServerCommunication.cs
@@ -106,7 +106,7 @@ namespace monshare.Utils
         }
         public static async Task<bool> CreateGroupAsync(string title, string description, int range, DateTime time, int targetNoPeople)
         {
-            var status = await CheckPermissions(Permission.Location);
+            var status = await Utils.CheckPermissions(Permission.Location);
 
             if (status != PermissionStatus.Granted)
             {
@@ -211,70 +211,6 @@ namespace monshare.Utils
             var json = await client.GetStringAsync(uri);
             var result = JsonValue.Parse(json);
             return result;
-        }
-
-        public static async Task<PermissionStatus> CheckPermissions(Permission permission)
-        {
-            var permissionStatus = await CrossPermissions.Current.CheckPermissionStatusAsync(permission);
-            bool request = false;
-            if (permissionStatus == PermissionStatus.Denied)
-            {
-                if (Device.RuntimePlatform == Device.iOS)
-                {
-
-                    var title = $"{permission} Permission";
-                    var question = $"To use this plugin the {permission} permission is required. Please go into Settings and turn on {permission} for the app.";
-                    var positive = "Settings";
-                    var negative = "Maybe Later";
-                    var task = Application.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
-                    if (task == null)
-                        return permissionStatus;
-
-                    var result = await task;
-                    if (result)
-                    {
-                        CrossPermissions.Current.OpenAppSettings();
-                    }
-
-                    return permissionStatus;
-                }
-
-                request = true;
-
-            }
-
-            if (request || permissionStatus != PermissionStatus.Granted)
-            {
-                var newStatus = await CrossPermissions.Current.RequestPermissionsAsync(permission);
-
-                if (!newStatus.ContainsKey(permission))
-                {
-                    return permissionStatus;
-                }
-
-                permissionStatus = newStatus[permission];
-
-                if (newStatus[permission] != PermissionStatus.Granted)
-                {
-                    permissionStatus = newStatus[permission];
-                    var title = $"{permission} Permission";
-                    var question = $"To use the plugin the {permission} permission is required.";
-                    var positive = "Settings";
-                    var negative = "Maybe Later";
-                    var task = Application.Current?.MainPage?.DisplayAlert(title, question, positive, negative);
-                    if (task == null)
-                        return permissionStatus;
-
-                    var result = await task;
-                    if (result)
-                    {
-                        CrossPermissions.Current.OpenAppSettings();
-                    }
-                    return permissionStatus;
-                }
-            }
-
-            return permissionStatus;
         }
 
     }
